ae7a81d9dba0b7acb4c3f126dd8d8617f6e22ac8
archana <archanajoshi99@gmail.com>
2021-05-09 15:36:10 +0530

    GetItems by popularity service method added

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/ItemService.java
@@ -0,0 +1,74 @@
+package com.upgrad.FoodOrderingApp.service.businness;
+
+import com.upgrad.FoodOrderingApp.service.dao.*;
+import com.upgrad.FoodOrderingApp.service.entity.*;
+import com.upgrad.FoodOrderingApp.service.exception.ItemNotFoundException;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+/* Service for Item Entity and OrderItem and Order and Restaurant and Category */
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemDao itemDao;
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private OrderItemDao orderItemDao;
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Autowired
+    RestaurantDao restaurantDao;
+
+    @Autowired
+    RestaurantItemDao restaurantItemDao;
+
+    /* Get Items By Popularity from Restaurant Entity,
+     * Check for Restaurant exists in DB, Return Sorted List
+     *  */
+    public List<ItemEntity> getItemsByPopularity(RestaurantEntity restaurantEntity) throws RestaurantNotFoundException {
+
+        if (restaurantEntity == null) {
+            throw new RestaurantNotFoundException("RNF-001", "No restaurant by this id");
+        }
+        List<OrderEntity> ordersEntities = orderDao.getOrderByRestaurant(restaurantEntity);
+
+        List<ItemEntity> itemEntities = new LinkedList<>();
+
+        ordersEntities.forEach(ordersEntity -> {
+            List<OrderItemEntity> orderItemEntities = orderItemDao.getItemsByOrders(ordersEntity);
+            orderItemEntities.forEach(orderItemEntity -> {
+                itemEntities.add(orderItemEntity.getItem());
+            });
+        });
+        Map<String, Integer> itemCountMap = new HashMap<>();
+        itemEntities.forEach(itemEntity -> {
+            Integer count = itemCountMap.get(itemEntity.getUuid());
+            itemCountMap.put(itemEntity.getUuid(), (count == null) ? 1 : count + 1);
+        });
+        Map<String, Integer> sortedItemCountMap = itemDao.sortMapByValues(itemCountMap);
+
+        List<ItemEntity> sortedItemEntites = new LinkedList<>();
+        Integer count = 0;
+        for (Map.Entry<String, Integer> item : sortedItemCountMap.entrySet()) {
+            if (count < 5) {
+                sortedItemEntites.add(itemDao.getItemByUuid(item.getKey()));
+                count = count + 1;
+            } else {
+                break;
+            }
+        }
+
+        return sortedItemEntites;
+    }
+}
